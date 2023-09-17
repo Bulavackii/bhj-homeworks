103f2482baa7afec9cfb7c9191e787cabc6b68d8
Denis B <bulavatsky@list.ru>
2023-09-17 17:11:34 +0300

    ready task 3

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,38 @@
+/*----------------- Task 3 -------------------*/
+
+let deadCount = 0; // счетчик убитых кротов
+let lostCount = 0; // счетчик промахов
+const targetCount = 10; // общее число кротов, которых нужно убить для победы
+const maxLostCount = 5; // количество поражений для завершения игры
+
+let getHole = index => document.getElementById(`hole${index}`);
+
+function handleClick(index) {
+    const hole = getHole(index);
+    if (hole.classList.contains('hole_has-mole')) {
+        // Крот убит
+        deadCount++;
+        hole.classList.remove('hole_has-mole'); // Убираем крота из лунки
+    } else {
+        // Промах
+        lostCount++;
+    }
+
+    // Обновляем текст в элементах счетчиков
+    document.getElementById("dead").textContent = deadCount;
+    document.getElementById("lost").textContent = lostCount;
+
+    // Проверяем условия победы и поражения
+    if (deadCount === targetCount) {
+        alert("Вы победили!");
+        resetGame();
+    } else if (lostCount === maxLostCount) {
+        alert("Игра окончена. Вы проиграли.");
+        resetGame();
+    }
+}
+
+// Добавляем обработчики клика к каждой лунке
+for (let i = 1; i <= 9; i++) {
+    getHole(i).addEventListener('click', () => handleClick(i));
+}
