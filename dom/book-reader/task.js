0cd81eb250e6be691552efef0474403c33bc9e8a
Denis B <bulavatsky@list.ru>
2023-09-30 18:52:08 +0300

    final work

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,7 +1,5 @@
-// Получите элементы управления размером шрифта
-const fontControls = document.querySelectorAll('.font-size');
 
-// Получите элемент читалки (книги)
+const fontControls = document.querySelectorAll('.font-size');
 const book = document.querySelector('.book');
 
 // Обработчик клика на элементах управления размером шрифта
@@ -9,21 +7,20 @@ fontControls.forEach(function(control) {
   control.addEventListener('click', function(event) {
     event.preventDefault();
     
-    // Удалите класс font-size_active со всех элементов управления
+    // Удаляем класс font-size_active со всех элементов управления
     fontControls.forEach(function(control) {
       control.classList.remove('font-size_active');
     });
     
-    // Установите класс font-size_active только на текущем элементе
+    // Устанавливаем класс font-size_active только на текущем элементе
     control.classList.add('font-size_active');
     
-    // Получите значение атрибута data-size для текущего элемента
+    // Получаем значение атрибута data-size для текущего элемента
     const size = control.getAttribute('data-size');
     
-    // Удалите все классы book_fs-*
     book.classList.remove('book_fs-big', 'book_fs-small');
     
-    // В зависимости от значения size, установите соответствующий класс на элемент книги
+    // В зависимости от значения size, устанавливаем соответствующий класс на элемент книги
     if (size === 'big') {
       book.classList.add('book_fs-big');
     } else if (size === 'small') {
