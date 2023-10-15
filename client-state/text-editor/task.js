faa84eef873881f13b5cf54e4f9358482a4fdeaa
Denis B <bulavatsky@list.ru>
2023-10-15 09:49:37 +0300

    edit task 1

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -3,11 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const editor = document.getElementById("editor");
     const clearButton = document.getElementById("clearButton");
 
-    // Загрузка текста из локального хранилища (если он там есть)
-    if (localStorage.getItem("editorText")) {
-        editor.value = localStorage.getItem("editorText");
-    }
-
     // Слушатель для сохранения текста в локальное хранилище при изменениях в текстовом поле
     editor.addEventListener("input", function () {
         const text = editor.value;
